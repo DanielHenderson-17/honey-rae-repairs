90a207ce6963c1255ce24d830f42cea3f44834a5
Daniel Henderson <djhindu17@gmail.com>
2024-09-10 09:32:40 -0500

    intro to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import "./App.css";
+import { NavBar } from "./components/nav/NavBar";
 import { CustomerList } from "./components/customers/CustomersList";
 import { EmployeeList } from "./components/employees/EmployeeList";
 import { TicketList } from "./components/tickets/TicketList";
-
+import { Routes, Route, Outlet } from "react-router-dom";
 
 export const App = () => {
-  return<>
-  <TicketList/>
-  <CustomerList/> 
-  <EmployeeList/>
-  </>
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+        <Route path="employees" element={<EmployeeList />} />
+      </Route>
+    </Routes>
+  );
 };
